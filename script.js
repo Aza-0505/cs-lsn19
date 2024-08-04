3f61d68f0a1310369350081b2aaf1130b16aaa87
Aza-0505 <kobakhidzeaza@gmail.com>
2024-08-04 10:29:05 +0400

    burger-bar

---
+++ script.js
@@ -0,0 +1,7 @@
+//burger
+const nav = document.getElementById('navigation');
+const burger = document.getElementById('burger-Bar');
+
+burger.addEventListener('click', function) {
+    nav.classList.toggle("active");
+}
